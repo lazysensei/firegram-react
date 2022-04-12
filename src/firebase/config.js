055a62f0e751afe7a3e79ffc6c277a2311060eb4
Olawale Ajakaye <ajakaye66@gmail.com>
2022-04-12 10:08:15 +0100

    Done

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/storage';
+import 'firebase/compat/firestore';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBX_hj8kjEA6o5ctxJSJSisiOGowkajWb0",
+    authDomain: "firegram-react-796ff.firebaseapp.com",
+    projectId: "firegram-react-796ff",
+    storageBucket: "firegram-react-796ff.appspot.com",
+    messagingSenderId: "413238392673",
+    appId: "1:413238392673:web:05b9ea6a31973323eb3720"
+  };
+
+firebase.initializeApp(firebaseConfig)
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectStorage, projectFirestore, timestamp };
